ac6bd32f830f03073415ac5833c69099f8694bee
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-11-02 15:49:27 -0400

    feat: main-page

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -10,52 +10,8 @@ import { CommonModule } from '@angular/common';
 })
 export class MainPageComponent {
   fileUploaded: boolean = false;
-  features = [
-    {
-      title: 'Automated Financial Summarization',
-      description: 'Generate concise summaries from complex financial reports quickly and efficiently.',
-      buttonText: 'Generate Summary',
-      action: () => this.generateSummary()
-    },
-    {
-      title: 'KPI Extraction',
-      description: 'Extract and visualize essential KPIs to monitor performance effectively.',
-      buttonText: 'Extract KPI',
-      action: () => this.extractKPI()
-    },
-    {
-      title: 'Data Analysis',
-      description: 'Utilize advanced analytics tools to uncover trends and insights from your financial data.',
-      buttonText: 'Analyze Data',
-      action: () => this.analyzeData()
-    },
-  ];
 
   startAnalysis() {
     this.fileUploaded = true;
-  }
-
-  onFileSelected(event: Event) {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    if (file) {
-      // Handle the uploaded file, e.g., parsing it or uploading it to a server
-      console.log('File selected:', file.name);
-      // Perform actions based on the uploaded file
-    }
-  }
-
-  generateSummary() {
-    // Logic for generating summary
-    console.log('Generating summary...');
-  }
-
-  extractKPI() {
-    // Logic for extracting KPI
-    console.log('Extracting KPI...');
-  }
-
-  analyzeData() {
-    // Logic for analyzing data
-    console.log('Analyzing data...');
   }
 }
